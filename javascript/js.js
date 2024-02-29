38a517fb938425e36e87f03b7e82e742ee03be3d
ikkitonga <mcgaston@gmail.com>
2024-02-29 16:31:54 -0300

    funcion expand y saludo

---
+++ javascript/js.js
@@ -0,0 +1,12 @@
+function expandCard(card) {
+    card.classList.toggle('expanded');
+  }
+
+
+let saludo = document.getElementById("prueba");
+
+function saludos(){
+    saludo.innerHTML = "Hola soy Gastón Macias";
+}
+
+saludos();
